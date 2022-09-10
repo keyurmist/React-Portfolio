edfbaeaea1904a5476bf8aeb3fd800a3d7819d64
KeyurMistry <keyurmist@gmail.com>
2022-09-10 17:22:51 +0100

    added components and imported to app.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,4 +1,9 @@
 import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 function App() {
   return (
@@ -7,7 +12,6 @@ function App() {
       <About />
       <Projects />
       <Skills />
-      <Testimonials />
       <Contact />
     </main>
   );
