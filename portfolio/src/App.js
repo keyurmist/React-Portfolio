258cf5fac1b776274a49781e1a28638f084208b9
KeyurMistry <keyurmist@gmail.com>
2022-09-10 17:33:53 +0100

    created about me section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -7,7 +7,7 @@ import Skills from "./components/Skills";
 
 function App() {
   return (
-    <main>
+    <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <About />
       <Projects />
