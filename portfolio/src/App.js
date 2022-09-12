be4b27937816e8e39143e3da658bf0b0d6d5d1d5
KeyurMistry <keyurmist@gmail.com>
2022-09-12 22:06:19 +0100

    created footer, installed daisyui

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,6 +4,7 @@ import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Projects />
       <Skills />
       <Contact />
+      <Footer />
     </main>
   );
 }
