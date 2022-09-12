57c37c28c771b109bd4755e3eb5452b7870e7f6f
KeyurMistry <keyurmist@gmail.com>
2022-09-12 22:57:00 +0100

    created footer with 3 account links

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -24,6 +24,7 @@ export default function Navbar() {
             CV
           </a>
         </nav>
+
         <a
           href="#contact"
           className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
